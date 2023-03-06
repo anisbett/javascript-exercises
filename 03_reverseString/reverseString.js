14294fd688b0b328ec2390daad04ceb3560a2b3b
anisbett <ajaninisbett@outlook.com>
2023-03-06 10:35:44 -0400

    reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+   const array = string.split('');
+    let num = array.length;
+    let newArray = [];
+    let reverseStr = ''
+    if (string === '') {
+        return reverseStr;
+    }
+    else {
+        for (let i =0; i < num; i++) {
+        newArray += array.pop();
+    }
+    reverseStr= newArray.toString();
+    return reverseStr;
+    }
 };
 
 // Do not edit below this line
